57f4e04b50841f9d01eb323a72dcf51054b45476
madinaabdulborieva <madinaabdulborieva@gmail.com>
2022-12-07 16:01:39 +0500

    create a function to count maximum number of unique consecutive chars in a string

--- dev-and-build-tools/Program.cs
+++ dev-and-build-tools/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+ 
+namespace Program { 
+ 
+    class Program{ 
+        public static void Main(string[] args) { 
+            Console.WriteLine("Enter some string: "); 
+            string? word = Console.ReadLine(); 
+            int count = 1; 
+            int max = 0; 
+            for(int i = 0; i < word.Length-1; i++) { 
+                while(i < word.Length-1 && word[i] != word[i+1]) { 
+                    count++; 
+                    i++; 
+                } 
+                if(max < count) { 
+                    max = count; 
+                } 
+                count = 1; 
+ 
+            } 
+            Console.WriteLine(max); 
+ 
+ 
+        } 
+    } 
+}
